15e8e881d3e59be67709ef985ad5db8cbcfa0b14
ascavarelli <user@server.com>
2011-01-30 10:23:21 -0500

    first commit

---
+++ include/Resources.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "cinder/CinderResources.h"
+
+#define BBlender_VERT	CINDER_RESOURCE( shaders/, BasicBlender_vert.glsl, 100, GLSL )
+#define BBlender_FRAG	CINDER_RESOURCE( shaders/, BasicBlender_frag.glsl, 101, GLSL )
+
+
